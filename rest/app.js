667466a83e55f3ced4f38f058d1ce2af0156d3b2
Zet Te Zet <dtue68@gmail.com>
2025-12-06 04:53:46 +0700

    zettezet-1

--- rest/app.js
+++ rest/app.js
@@ -1,10 +1,28 @@
+var listCoursesBlock = document.querySelector('#list-courses');
 var courseApi = 'http://localhost:3000/courses';
-fetch(courseApi)
-.then(function(response)
-{
-    return response.json();
-})
-.then(function(courses)
-{
-    console.log(courses);
-})
+
+
+function start() {
+    getCourses(function(courses) {;
+        renderCourses(courses);
+    });
+}
+start();
+
+
+function getCourses(callback) {
+    fetch(courseApi)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(callback);
+}
+    function renderCourses(courses) {
+        var htmls = courses.map(function(course) {
+            return `<li>
+                <h2>${course.name}</h2>
+                <p>${course.description}</p>
+            </li>`;
+        });
+        listCoursesBlock.innerHTML = htmls.join('');
+}
